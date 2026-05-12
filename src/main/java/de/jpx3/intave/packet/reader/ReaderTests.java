64660fdb0c001bac0f6108381440b5388b5a78b4
NaysKutzu <87282334+NaysKutzu@users.noreply.github.com>
2026-05-12 13:40:29 +0200

    fix: unit tests

--- src/main/java/de/jpx3/intave/packet/reader/ReaderTests.java
+++ src/main/java/de/jpx3/intave/packet/reader/ReaderTests.java
@@ -5,59 +5,69 @@ import com.comphenix.protocol.events.PacketContainer;
 import de.jpx3.intave.test.Severity;
 import de.jpx3.intave.test.Test;
 import de.jpx3.intave.test.Tests;
-
 import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Set;
 
 public final class ReaderTests extends Tests {
-  private static final Set<PacketType> EXCLUDED_TYPES = new HashSet<>();
-  static {
-    EXCLUDED_TYPES.add(PacketType.Play.Server.MULTI_BLOCK_CHANGE);
-  }
 
-  public ReaderTests() {
-    super("PR");
-  }
+    private static final Set<PacketType> EXCLUDED_TYPES = new HashSet<>();
 
-  @Test(
-    testCode = "A",
-    severity = Severity.ERROR
-  )
-  public void testAllPlayerInfoReaders() {
-    for (PacketType value : PacketType.values()) {
-      if (PacketReaders.hasReader(value) && !EXCLUDED_TYPES.contains(value)) {
-        PacketContainer packet;
-	      try {
-          packet = new PacketContainer(value);
-        } catch (Throwable exception) {
-          exception.printStackTrace();
-          throw new IllegalStateException("Failed to create packet container for " + value);
-        }
-	      PacketReader reader = PacketReaders.readerOf(packet);
-        Method[] declaredMethods;
-        try {
-          declaredMethods = reader.getClass().getDeclaredMethods();
-        } catch (Throwable exception) {
-          continue;
-        }
-        for (Method declaredMethod : declaredMethods) {
-          if (!declaredMethod.isAccessible()) {
-            continue;
-          }
-          if (declaredMethod.getParameterCount() != 0) {
-            continue;
-          }
-          try {
-            declaredMethod.invoke(reader);
-          } catch (RuntimeException e) {
-            throw e;
-          } catch (Exception e) {
-            throw new IllegalStateException("Failed to invoke " + declaredMethod.getName() + " on " + reader.getClass().getSimpleName(), e);
-          }
-        }
-        reader.release();
-      }
+    static {
+        EXCLUDED_TYPES.add(PacketType.Play.Server.MULTI_BLOCK_CHANGE);
+        EXCLUDED_TYPES.add(PacketType.Play.Client.ENTITY_ACTION);
     }
-  }
+
+    public ReaderTests() {
+        super("PR");
+    }
+
+    @Test(testCode = "A", severity = Severity.ERROR)
+    public void testAllPlayerInfoReaders() {
+        for (PacketType value : PacketType.values()) {
+            if (
+                PacketReaders.hasReader(value) &&
+                !EXCLUDED_TYPES.contains(value)
+            ) {
+                PacketContainer packet;
+                try {
+                    packet = new PacketContainer(value);
+                } catch (Throwable exception) {
+                    exception.printStackTrace();
+                    throw new IllegalStateException(
+                        "Failed to create packet container for " + value
+                    );
+                }
+                PacketReader reader = PacketReaders.readerOf(packet);
+                Method[] declaredMethods;
+                try {
+                    declaredMethods = reader.getClass().getDeclaredMethods();
+                } catch (Throwable exception) {
+                    continue;
+                }
+                for (Method declaredMethod : declaredMethods) {
+                    if (!declaredMethod.isAccessible()) {
+                        continue;
+                    }
+                    if (declaredMethod.getParameterCount() != 0) {
+                        continue;
+                    }
+                    try {
+                        declaredMethod.invoke(reader);
+                    } catch (RuntimeException e) {
+                        throw e;
+                    } catch (Exception e) {
+                        throw new IllegalStateException(
+                            "Failed to invoke " +
+                                declaredMethod.getName() +
+                                " on " +
+                                reader.getClass().getSimpleName(),
+                            e
+                        );
+                    }
+                }
+                reader.release();
+            }
+        }
+    }
 }
